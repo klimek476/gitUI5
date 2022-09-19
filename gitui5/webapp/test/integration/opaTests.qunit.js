b9fa510b003f9df4d14e570963455a61b6808698
MichalKlimczak <michal_klimczak@onet.pl>
2022-09-19 14:39:52 +0200

    initial commit of UI5 ap

---
+++ gitui5/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["gitui5/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ gitui5/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"gitui5/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
